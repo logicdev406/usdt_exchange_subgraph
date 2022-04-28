9ee97cc1177077d49a8a9434aa7242f31d958124
Sunday Ochuko <sundayochuko101@gmail.com>
2022-04-28 20:24:21 +0100

    first commit

---
+++ mapping/transfer.ts
@@ -0,0 +1,16 @@
+import { Exchange } from "../generated/schema";
+import { Transfer } from "../generated/Transfer/USDT";
+
+export function handleTransfer(event: Transfer): void {
+    let transfer = Exchange.load(event.transaction.hash.toHex());
+    if (transfer == null) {
+        transfer = new Exchange(event.transaction.hash.toHex())
+    }
+
+    transfer.block = event.block.number;
+    transfer.from = event.params.from.toHex();
+    transfer.to = event.params.to.toHex();
+    transfer.amount = event.params.value;
+
+    transfer.save()
+}
